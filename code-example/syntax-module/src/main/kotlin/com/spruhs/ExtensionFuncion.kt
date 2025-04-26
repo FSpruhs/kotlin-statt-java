20dd45f6aede253663dbec3a93260d299ad6e52f
spruhs <fabian@spruhs.com>
2025-04-26 20:16:31 +0200

    feedback lisa teil 1

--- code-example/syntax-module/src/main/kotlin/com/spruhs/ExtensionFuncion.kt
+++ code-example/syntax-module/src/main/kotlin/com/spruhs/ExtensionFuncion.kt
@@ -1,10 +1,10 @@
 package main.kotlin.com.spruhs
 
-fun String.double(): String {
+fun String.repeat(): String {
     return "$this $this"
 }
 
 fun main() {
-    val name = "Hello World!".double()
-    println(name.double()) // Output: Hello World! Hello World! Hello World! Hello World!
+    val name = "Hello World!".repeat()
+    println(name.repeat()) // Output: Hello World! Hello World! Hello World! Hello World!
 }
